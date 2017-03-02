591782ad9fc3792a234b4c8ee6058848108a28ad
germanio <german.hohmann@gmail.com>
2017-03-02 02:19:14 -0300

    Agrega capítulo de uso de datos reales

--- public/app/app.config.js
+++ public/app/app.config.js
@@ -1,16 +1,16 @@
-angular.
-  module('ptfApp').
-  config(['$locationProvider', '$routeProvider',
-    function config($locationProvider, $routeProvider) {
-      $locationProvider.hashPrefix('!');
+angular
+    .module('ptfApp')
+    .config(['$locationProvider', '$routeProvider',
+        function config($locationProvider, $routeProvider) {
+            $locationProvider.hashPrefix('!');
 
-      $routeProvider.
-        when('/estaciones', {
-          template: '<estaciones></estaciones>'
-        }).
-        when('/estaciones/:id', {
-          template: '<detalle-estacion></detalle-estacion>'
-        }).
-        otherwise('/estaciones');
-    }
-  ]);
+            $routeProvider.
+                when('/estaciones', {
+                  template: '<estaciones></estaciones>'
+                }).
+                when('/estaciones/:id', {
+                  template: '<detalle-estacion></detalle-estacion>'
+                }).
+                otherwise('/estaciones');
+        }
+]);
