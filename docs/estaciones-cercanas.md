0cba2079f4914785e9b1b043efa24f3f93b502d1
Germán <german.hohmann@gmail.com>
2017-09-17 14:14:01 -0300

    Capítulo de estaciones cercanas
    Agrega funcionalidad inicial:
    - la primera versión del filtro para obtener las estaciones
    cercanas (filtrado y ordenamiento)
    - binding con el html de la lista de estaciones
    - algo de texto en el doc correspondiente.

---
+++ docs/estaciones-cercanas.md
@@ -0,0 +1,6 @@
+# Las estaciones más cercanas
+
+ Cómo haríamos para obtener las estaciones más cercanas a un lugar de la ciudad, con los datos que tenemos?
+ 
+
+ 
---
+++ public/app/proximidad.filter.js
@@ -0,0 +1,49 @@
+angular.
+  module('ptfApp').
+  filter('porProximidad', function() {
+    
+    function radianes_a_metros(coordenadas){
+        const RAD = 0.000008998719243599958;
+        //calculamos la distancia al centro usando pitágoras
+        return Math.round(Math.sqrt(Math.pow(coordenadas.latitud, 2) + Math.pow(coordenadas.longitud, 2)) / RAD);
+    }
+    
+    return function(estaciones) {
+        var estacionesFiltradas = [];
+        
+        //coordenadas del obelisco
+        var centro_coords = {
+            latitud: -34.6033,
+            longitud: -58.3816
+        };
+        
+        //distancia máxima en metros
+        var distanciaMaxima = 1000;
+        
+        //recorremos las estaciones para ver cual se queda y cual se va
+        angular.forEach(estaciones, function(estacion) {
+            //obtenemos el valor de latitud y longitud:
+            var estacion_coords = {
+                latitud: parseFloat(estacion.Latitud),
+                longitud: parseFloat(estacion.Longitud)
+            };
+            
+            var distancia = radianes_a_metros({
+                latitud: centro_coords.latitud - estacion_coords.latitud,
+                longitud: centro_coords.longitud - estacion_coords.longitud
+            });
+            
+            //éste valor debe ser menor que el cuadrado de la distancia máxima
+            if (distanciaMaxima > distancia) {
+                estacionesFiltradas.push(estacion);
+                
+                //guardamos la distancia entre el centro y la estación para poder ordenar por proximidad
+                estacion.distancia = distancia;
+            }
+        });
+        //ahora si, antes de devolver las estaciones filtradas, tenemos que ordenarlas por proximidad
+        return estacionesFiltradas.sort(function(e1, e2) {
+            return e1.distancia - e2.distancia;
+        });
+    };
+  });
